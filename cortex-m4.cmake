d54ea3f6ce8aef4c56af87ad2596a3b1c023c13a
Jake Swensen <jake@swensen.io>
2021-06-21 15:09:38 -0500

    feat: add cortex-m4 and cortex m4f

---
+++ cortex-m4.cmake
@@ -0,0 +1,10 @@
+# Name of the target
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
+
+set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4")
+set(VFP_FLAGS "")
+set(LD_FLAGS "-nostartfiles")
+
+include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi.cmake)
